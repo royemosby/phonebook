fc144ae3b011737758640eec9c0eaeaa8ef15791
Roy Mosby <ego.madking@gmail.com>
2025-01-29 13:27:21 -0500

    remove contacts and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { useState } from 'react';
 import './App.css';
-import importedContacts from './assets/contacts.json';
 import Phonebook from './Phonebook';
 
-const sortedContacts = importedContacts.sort((a, b) =>
-  a.lastName.localeCompare(b.lastName)
-);
-
 function App() {
-  const [contacts] = useState(sortedContacts);
   return (
     <>
       <main>
         <h1>CTD Phone Book</h1>
-        <Phonebook contacts={contacts} />
+        <Phonebook />
       </main>
     </>
   );
